489edf25b133dbe35c27190e5f9249163669490e
ltt <leitingting08@gmail.com>
2020-04-16 00:18:11 +0800

    feat: 初始化弹幕

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,48 +1,26 @@
 <template>
   <div class="index">
-    <div>
-      <i class="iconfont icon-users" />
+    <div class="title">弹幕测试标题</div>
+    <div class="video-container">
+      <video ref="videoPlayer" src="~@/assets/test.mp4" preload="auto" class="video"></video>
+      <canvas ref="canvas" width="952" height="532" class="canvas"></canvas>
     </div>
-    <!-- <video-player ref="videoPlayer" :options="playerOptions"></video-player> -->
-    <canvas ref="canvas"></canvas>
-    <div>
-      <i class="iconfont icon-avatar" />
-      <input type="text" @keyup.prevent.stop.enter="send" v-model="content" />
-      <button type="submit">发送弹幕</button>
+
+   <div class="footer">
+      <div class="flex">
+        <i class="iconfont icon-avatar" />
+        <el-input v-model="inputMsg" @keyup.enter.native="sendMsg" />
+        <el-button type="primary" @click="sendMsg">发送弹幕</el-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-let audio = require('../assets/test.mp4')
 export default {
   name: 'Index',
   data() {
     return {
-      playerOptions: {
-        playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
-        autoplay: false, // 如果为true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 是否视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: 'zh-CN',
-        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [
-          {
-            type: 'video/mp4', // 类型
-            src: audio, // url地址
-          },
-        ],
-        poster: '', // 封面地址
-        notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true, // 当前时间和持续时间的分隔符
-          durationDisplay: true, // 显示持续时间
-          remainingTimeDisplay: false, // 是否显示剩余时间功能
-          fullscreenToggle: true, // 是否显示全屏按钮
-        },
-      },
       barrageList: [],
       canvas: null,
       w: null,
@@ -58,41 +36,7 @@ export default {
     this.h = rect.bottom - rect.top
     this.ctx = this.canvas.getContext('2d')
 
-    const textList = [
-      '弹幕',
-      '666',
-      '233333333',
-      'javascript',
-      'html',
-      'css',
-      '前端框架',
-      'Vue',
-      'React',
-      'Angular',
-      '测试弹幕效果',
-      '弹幕',
-      '666',
-      '233333333',
-      'javascript',
-      'html',
-      'css',
-      '前端框架',
-      'Vue',
-      'React',
-      'Angular',
-      '测试弹幕效果',
-      '弹幕',
-      '666',
-      '233333333',
-      'javascript',
-      'html',
-      'css',
-      '前端框架',
-      'Vue',
-      'React',
-      'Angular',
-      '测试弹幕效果',
-    ]
+    const textList = ['富强民主文明和谐公正法治爱国敬业诚信友善','富强民主文明和谐公正法治爱国敬业诚信友善','富强民主文明和谐公正法治爱国敬业诚信友善','富强民主文明和谐公正法治爱国敬业诚信友善','富强民主文明和谐公正法治爱国敬业诚信友善','富强民主文明和谐公正法治爱国敬业诚信友善','富强民主文明和谐公正法治爱国敬业诚信友善']
     this.draw()
     textList.forEach((t) => {
       this.shoot(t)
@@ -113,6 +57,7 @@ export default {
         color: color,
         offset: offset,
         width: width,
+        font: '30px Arial'
       }
       this.barrageList.push(barrage)
     },
@@ -136,6 +81,7 @@ export default {
     //绘制文字
     drawText(barrage) {
       this.ctx.fillStyle = barrage.color
+      this.ctx.font = barrage.font
       this.ctx.fillText(barrage.value, barrage.left, barrage.top)
     },
 
@@ -160,18 +106,31 @@ export default {
 
 <style lang="less" scoped>
 .index {
-  input {
-    width: 80%;
-    height: 24px;
-    margin-left: 10px;
+  .title{
+    padding: 10px;
   }
-  button {
-    height: 28px;
-    border-radius: 2em;
-    background: cornflowerblue;
-    color: #fff;
-    border: 0;
-    margin-left: 10px;
+  .video-container {
+    position: relative;
+    width: 952px;
+    height: 532px;
+    margin: auto;
+    .video {
+      width: 100%;
+      height: 100%;
+    }
+    .canvas {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+  .footer{
+    width: 940px;
+    margin: auto;
+    .iconfont{font-size: 40px;margin-right: 5px;color:cornflowerblue;}
+    .flex{
+      display: flex;
+    }
   }
 }
 </style>
