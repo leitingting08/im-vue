1dccce89c03506977e29d47e20aab0b8280c8ccb
leitingting08 <leitingting08@gmail.com>
2020-04-28 00:36:01 +0800

    feat: 首页

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,114 +1,26 @@
 <template>
   <div class="index">
-    <!-- 视频+弹幕 -->
-    <div class="index-container">
+    <div class="index-container" v-for="({name,title,img,avatar},index) in data" :key="index">
       <!-- 标题 -->
       <div class="index-container-title">
         <div class="l-con">
           <svg class="live" viewBox="0 0 1024 1024"><path d="M392.448 275.911111a92.416 92.416 0 1 1-184.832 0 92.416 92.416 0 0 1 184.832 0" fill="#23ADE5"></path><path d="M826.624 464.583111l-63.744 36.864v-48.64a72.206222 72.206222 0 0 0-71.68-71.936H190.72a72.192 72.192 0 0 0-71.936 71.936V748.231111a71.936 71.936 0 0 0 71.936 71.936H691.2a71.936 71.936 0 0 0 71.936-71.936v-23.808l63.488 37.888a51.2 51.2 0 0 0 76.8-44.544V508.871111a51.2 51.2 0 0 0-76.8-44.288M572.928 369.351111c79.459556 0.142222 143.985778-64.156444 144.128-143.616 0.142222-79.459556-64.156444-143.985778-143.616-144.128-79.260444-0.142222-143.701333 63.857778-144.128 143.104-0.426667 79.459556 63.644444 144.213333 143.104 144.64h0.512" fill="#48CFE5"></path><path d="M425.216 512.967111l124.16 71.936a25.6 25.6 0 0 1 0 42.496l-124.16 71.68a25.6 25.6 0 0 1-37.12-21.248V534.471111a25.6 25.6 0 0 1 37.12-21.504" fill="#FDDE80"></path></svg>
-          <span class="name">动画</span>
+          <span class="name">{{ title }}</span>
         </div>
       </div>
       <!-- 房间块 -->
       <div class="index-container-grid">
-        <div class="block" v-for="i in 8" :key="i" @click="openRoom(i,'room')">
+        <div class="block" v-for="i in 8" :key="i" @click="openRoom(i, name)">
           <div class="pic">
-            <img src="~@/assets/pic.webp" alt="">
+            <img :src="img" alt="">
             <div class="count">
               <i class="iconfont icon-users" /> 250.5万
             </div>
           </div>
           <div class="up">
-            <img src="~@/assets/avatar.jpg" alt="">
+            <img :src="avatar" alt="">
             <div class="txt">
-              <p class="name">LexBurner</p>
-              <p title="小学生来啦！" class="desc">小学生来啦！</p>
-              <p class="tag">王者荣耀</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- 聊天 -->
-    <div class="index-container">
-      <!-- 标题 -->
-      <div class="index-container-title">
-        <div class="l-con">
-          <svg class="live" viewBox="0 0 1024 1024"><path d="M392.448 275.911111a92.416 92.416 0 1 1-184.832 0 92.416 92.416 0 0 1 184.832 0" fill="#23ADE5"></path><path d="M826.624 464.583111l-63.744 36.864v-48.64a72.206222 72.206222 0 0 0-71.68-71.936H190.72a72.192 72.192 0 0 0-71.936 71.936V748.231111a71.936 71.936 0 0 0 71.936 71.936H691.2a71.936 71.936 0 0 0 71.936-71.936v-23.808l63.488 37.888a51.2 51.2 0 0 0 76.8-44.544V508.871111a51.2 51.2 0 0 0-76.8-44.288M572.928 369.351111c79.459556 0.142222 143.985778-64.156444 144.128-143.616 0.142222-79.459556-64.156444-143.985778-143.616-144.128-79.260444-0.142222-143.701333 63.857778-144.128 143.104-0.426667 79.459556 63.644444 144.213333 143.104 144.64h0.512" fill="#48CFE5"></path><path d="M425.216 512.967111l124.16 71.936a25.6 25.6 0 0 1 0 42.496l-124.16 71.68a25.6 25.6 0 0 1-37.12-21.248V534.471111a25.6 25.6 0 0 1 37.12-21.504" fill="#FDDE80"></path></svg>
-          <span class="name">聊天室</span>
-        </div>
-      </div>
-      <!-- 房间块 -->
-      <div class="index-container-grid">
-        <div class="block" v-for="i in 8" :key="i" @click="openRoom(i,'chat')">
-          <div class="pic">
-            <img src="~@/assets/pic.webp" alt="">
-            <div class="count">
-              <i class="iconfont icon-users" /> 250.5万
-            </div>
-          </div>
-          <div class="up">
-            <img src="~@/assets/avatar1.gif" alt="">
-            <div class="txt">
-              <p class="name">LexBurner</p>
-              <p title="小学生来啦！" class="desc">小学生来啦！</p>
-              <p class="tag">王者荣耀</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- 直播课堂 -->
-     <div class="index-container">
-      <!-- 标题 -->
-      <div class="index-container-title">
-        <div class="l-con">
-          <svg class="live" viewBox="0 0 1024 1024"><path d="M392.448 275.911111a92.416 92.416 0 1 1-184.832 0 92.416 92.416 0 0 1 184.832 0" fill="#23ADE5"></path><path d="M826.624 464.583111l-63.744 36.864v-48.64a72.206222 72.206222 0 0 0-71.68-71.936H190.72a72.192 72.192 0 0 0-71.936 71.936V748.231111a71.936 71.936 0 0 0 71.936 71.936H691.2a71.936 71.936 0 0 0 71.936-71.936v-23.808l63.488 37.888a51.2 51.2 0 0 0 76.8-44.544V508.871111a51.2 51.2 0 0 0-76.8-44.288M572.928 369.351111c79.459556 0.142222 143.985778-64.156444 144.128-143.616 0.142222-79.459556-64.156444-143.985778-143.616-144.128-79.260444-0.142222-143.701333 63.857778-144.128 143.104-0.426667 79.459556 63.644444 144.213333 143.104 144.64h0.512" fill="#48CFE5"></path><path d="M425.216 512.967111l124.16 71.936a25.6 25.6 0 0 1 0 42.496l-124.16 71.68a25.6 25.6 0 0 1-37.12-21.248V534.471111a25.6 25.6 0 0 1 37.12-21.504" fill="#FDDE80"></path></svg>
-          <span class="name">直播课堂</span>
-        </div>
-      </div>
-      <!-- 房间块 -->
-      <div class="index-container-grid">
-        <div class="block" v-for="i in 8" :key="i" @click="openRoom(i)">
-          <div class="pic">
-            <img src="~@/assets/pic.webp" alt="">
-            <div class="count">
-              <i class="iconfont icon-users" /> 250.5万
-            </div>
-          </div>
-          <div class="up">
-            <img src="~@/assets/avatar2.jpg" alt="">
-            <div class="txt">
-              <p class="name">LexBurner</p>
-              <p title="小学生来啦！" class="desc">小学生来啦！</p>
-              <p class="tag">王者荣耀</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- 弹幕直播 -->
-     <div class="index-container">
-      <!-- 标题 -->
-      <div class="index-container-title">
-        <div class="l-con">
-          <svg class="live" viewBox="0 0 1024 1024"><path d="M392.448 275.911111a92.416 92.416 0 1 1-184.832 0 92.416 92.416 0 0 1 184.832 0" fill="#23ADE5"></path><path d="M826.624 464.583111l-63.744 36.864v-48.64a72.206222 72.206222 0 0 0-71.68-71.936H190.72a72.192 72.192 0 0 0-71.936 71.936V748.231111a71.936 71.936 0 0 0 71.936 71.936H691.2a71.936 71.936 0 0 0 71.936-71.936v-23.808l63.488 37.888a51.2 51.2 0 0 0 76.8-44.544V508.871111a51.2 51.2 0 0 0-76.8-44.288M572.928 369.351111c79.459556 0.142222 143.985778-64.156444 144.128-143.616 0.142222-79.459556-64.156444-143.985778-143.616-144.128-79.260444-0.142222-143.701333 63.857778-144.128 143.104-0.426667 79.459556 63.644444 144.213333 143.104 144.64h0.512" fill="#48CFE5"></path><path d="M425.216 512.967111l124.16 71.936a25.6 25.6 0 0 1 0 42.496l-124.16 71.68a25.6 25.6 0 0 1-37.12-21.248V534.471111a25.6 25.6 0 0 1 37.12-21.504" fill="#FDDE80"></path></svg>
-          <span class="name">弹幕直播</span>
-        </div>
-      </div>
-      <!-- 房间块 -->
-      <div class="index-container-grid">
-        <div class="block" v-for="i in 8" :key="i" @click="openRoom(i)">
-          <div class="pic">
-            <img src="~@/assets/pic.webp" alt="">
-            <div class="count">
-              <i class="iconfont icon-users" /> 250.5万
-            </div>
-          </div>
-          <div class="up">
-            <img src="~@/assets/avatar3.jpg" alt="">
-            <div class="txt">
-              <p class="name">LexBurner</p>
+              <p class="name">{{ name }}</p>
               <p title="小学生来啦！" class="desc">小学生来啦！</p>
               <p class="tag">王者荣耀</p>
             </div>
@@ -120,10 +32,25 @@
 </template>
 
 <script>
+const img0 = require('@/assets/pic.webp')
+const img1 = require('@/assets/chat1.gif')
+const img2 = require('@/assets/course.webp')
+const img3 = require('@/assets/chat.jpg')
+const avatar0 = require('@/assets/avatar.jpg')
+const avatar1 = require('@/assets/avatar1.gif')
+const avatar2 = require('@/assets/avatar2.jpg')
+const avatar3 = require('@/assets/avatar3.jpg')
+
 export default {
   name: 'Index',
   data() {
     return {
+      data:[
+        {name: 'room',title:'动画',img: img0, avatar: avatar0},
+        {name: 'chat',title:'聊天室',img: img1, avatar: avatar1},
+        {name: 'course',title:'直播课堂',img: img2, avatar: avatar2},
+        {name: 'live',title:'游戏直播',img: img3, avatar: avatar3}
+      ]
     }
   },
   mounted() {
